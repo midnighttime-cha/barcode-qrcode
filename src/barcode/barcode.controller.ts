1ce2febbceb52b0f43b977227f3041c69181d1a1
Chawalit Sengsang <chawalit1987@gmail.com>
2021-10-05 19:31:19 +0700

    first commit

--- src/barcode/barcode.controller.ts
+++ src/barcode/barcode.controller.ts
@@ -1,6 +1,11 @@
-import { Controller, Get, Logger, Query, Res } from '@nestjs/common';
-import { ApiQuery, ApiParam } from '@nestjs/swagger';
+import { Controller, Get, HttpException, HttpStatus, Logger, Query, Res, UseInterceptors } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { ApiQuery } from '@nestjs/swagger';
 import { BarcodeService } from './barcode.service';
+const fs = require('fs');
+const JsBarcode = require('jsbarcode');
+const { createCanvas } = require("canvas");
+const base64ToImage = require('base64-to-image');
 
 @Controller('barcode')
 export class BarcodeController {
@@ -8,15 +13,15 @@ export class BarcodeController {
     private barcodeService: BarcodeService
   ) { }
 
-  @ApiQuery({ name: 'type' })
   @ApiQuery({ name: 'text' })
-  @ApiQuery({ name: 'code' })
   @Get()
   async genBarcode(@Res() res, @Query() query) {
-    const responseData = await this.barcodeService.genBarcode(query);
-    res.write('<html><body>');
-    res.write(`<img src="${responseData}" />`);
-    res.write('</body></html>');
-    return res.end();
+    try {
+      const responseData = await this.barcodeService.genBarcode(query);
+      await fs.writeFileSync(`./files/barcode/${query.text}.png`, responseData, 'base64');
+      return await res.status(HttpStatus.OK).sendFile(`${query.text}.png`, { root: "./files/barcode/" });
+    } catch (error) {
+      throw new HttpException(`[generate barcode] => ${error.message}`, HttpStatus.BAD_REQUEST);
+    }
   }
 }
--- src/barcode/barcode.service.ts
+++ src/barcode/barcode.service.ts
@@ -1,80 +1,82 @@
 import { HttpException, HttpStatus, Injectable, Logger } from '@nestjs/common';
 const JsBarcode = require('jsbarcode');
+const { createCanvas } = require("canvas");
+const base64ToImage = require('base64-to-image');
 
 @Injectable()
 export class BarcodeService {
   async genBarcode(query: any = null) {
     try {
       if (query) {
-        const { format, code, text, background, lineColor, textMargin, textAlign, textPosition, margin, marginBottom, marginTop, fontOptions, font, fontSize } = query;
-        let options = {};
-
-        if (typeof format !== "undefined") {
-          options['format'] = format;
-        } else {
-          options['format'] = "CODE128";
-        }
+        const { format, text, background, lineColor, textMargin, textAlign, textPosition, margin, marginBottom, marginTop, fontOptions, font, fontSize } = query;
 
         if (typeof text !== "undefined") {
-          options['text'] = text.split('\n').join(' ');
-        }
+          let options = {};
+          const canvasDOM = await createCanvas();
 
-        if (typeof background !== "undefined") {
-          options['background'] = `#${background}`;
+          if (typeof format !== "undefined") {
+            options['format'] = format;
+          } else {
+            options['format'] = "CODE128";
+          }
+
+          if (typeof text !== "undefined") {
+            options['text'] = text.split('\n').join(' ');
+          }
+
+          if (typeof background !== "undefined") {
+            options['background'] = `#${background}`;
+          } else {
+            options['background'] = `#ffffff`;
+          }
+
+          if (typeof lineColor !== "undefined") {
+            options['lineColor'] = `#${lineColor}`;
+          }
+
+          if (typeof textAlign !== "undefined") {
+            options['textAlign'] = textAlign;
+          }
+
+          if (typeof textPosition !== "undefined") {
+            options['textPosition'] = textPosition;
+          }
+
+          if (typeof margin !== "undefined") {
+            options['margin'] = margin;
+          }
+
+          if (typeof marginBottom !== "undefined") {
+            options['marginBottom'] = marginBottom;
+          }
+
+          if (typeof marginTop !== "undefined") {
+            options['marginTop'] = marginTop;
+          }
+
+          if (typeof textMargin !== "undefined") {
+            options['textMargin'] = textMargin;
+          }
+
+          if (typeof fontOptions !== "undefined") {
+            options['fontOptions'] = fontOptions;
+          }
+
+          if (typeof font !== "undefined") {
+            options['font'] = font;
+          }
+
+          if (typeof fontSize !== "undefined") {
+            options['fontSize'] = fontSize;
+          }
+
+          await JsBarcode(canvasDOM, text, options);
+
+          // return await base64ToImage(`${await canvasDOM.toDataURL('image/png')}`, "./files/barcode/", { 'fileName': `${query.text}.png`, 'type': 'png' });
+          return await `${canvasDOM.toDataURL('image/png')}`.replace(/^data:image\/png;base64,/, "");
         } else {
-          options['background'] = `#ffffff`;
+          throw new HttpException("Undefined param require.", HttpStatus.BAD_REQUEST);
         }
-
-        if (typeof lineColor !== "undefined") {
-          options['lineColor'] = `#${lineColor}`;
-        }
-
-        if (typeof textAlign !== "undefined") {
-          options['textAlign'] = textAlign;
-        }
-
-        if (typeof textPosition !== "undefined") {
-          options['textPosition'] = textPosition;
-        }
-
-        if (typeof margin !== "undefined") {
-          options['margin'] = margin;
-        }
-
-        if (typeof marginBottom !== "undefined") {
-          options['marginBottom'] = marginBottom;
-        }
-
-        if (typeof marginTop !== "undefined") {
-          options['marginTop'] = marginTop;
-        }
-
-        if (typeof textMargin !== "undefined") {
-          options['textMargin'] = textMargin;
-        }
-
-        if (typeof fontOptions !== "undefined") {
-          options['fontOptions'] = fontOptions;
-        }
-
-        if (typeof font !== "undefined") {
-          options['font'] = font;
-        }
-
-        if (typeof fontSize !== "undefined") {
-          options['fontSize'] = fontSize;
-        }
-
-
-        const { createCanvas } = require("canvas");
-        const canvasDOM = createCanvas(200, 200);
-
-        JsBarcode(canvasDOM, `${code}`, { ...options, flat: true });
-        Logger.log(canvasDOM, "canvasDOM");
-
-        return canvasDOM.toDataURL();
-      } else {
-        throw new HttpException("Undefined param require.", HttpStatus.BAD_REQUEST);
       }
     } catch (error) {
       throw new HttpException(`[JsBarcode fail.] => ${error.message}`, HttpStatus.BAD_REQUEST);
